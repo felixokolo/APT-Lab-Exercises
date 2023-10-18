c21ee17a6cb4336569e4ef60b22f6b7001e28757
felixokolo <pheelix.info@gmail.com>
2023-10-18 19:22:06 +0200

    First commit to test Eclipse Git environment

---
+++ Coordinate/myCode/CCoordinate.cpp
@@ -0,0 +1,20 @@
+/*
+ * CCoordinate.cpp
+ *
+ *  Created on: 18 Oct 2023
+ *      Author: FELIX WORKSTATION
+ */
+
+#include "CCoordinate.h"
+
+CCoordinate::CCoordinate()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CCoordinate::~CCoordinate()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Coordinate/myCode/CCoordinate.h
@@ -0,0 +1,18 @@
+/*
+ * CCoordinate.h
+ *
+ *  Created on: 18 Oct 2023
+ *      Author: FELIX WORKSTATION
+ */
+
+#ifndef CCOORDINATE_H_
+#define CCOORDINATE_H_
+
+class CCoordinate
+{
+public:
+	CCoordinate();
+	virtual ~CCoordinate();
+};
+
+#endif /* CCOORDINATE_H_ */
